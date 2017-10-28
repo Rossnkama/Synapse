04204e58027717d3bbc8d0809f07689d127156b9
Ross Nkama <ross.nkama@adacollege.org.uk>
2017-10-28 20:04:34 +0100

    Can tokenise key characters

--- src/Lexer.java
+++ src/Lexer.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;     import java.util.List;
 
 public class Lexer {
 
+    static List tokens = new ArrayList<>();
+
     private static String openFile(String path_to_file) throws Exception{
 
         // Initialising the string that returns out output value
@@ -17,7 +19,7 @@ public class Lexer {
             // While the lines aren't nothing, add them to opened file
             while ((line = br.readLine()) != null) opened_file += line;
 
-        // Exception handling
+            // Exception handling
         } catch (IOException e) {
 
             System.out.println("File not Found");
@@ -34,37 +36,56 @@ public class Lexer {
         List keyWords = new ArrayList<>();
         keyWords.add("out"); keyWords.add("if"); keyWords.add("def"); keyWords.add("for");
         String builder = "";
-        List tokens = new ArrayList<>();
         boolean stringState = false;
         String string = "";
 
         for (int i = 0; i < opened_file.length(); i++) {
             // Our reader which can identify the character at any time.
             char chr = opened_file.charAt(i);
-            switch (chr) {
-                case '"':
-                    // If stringState is true...
-                    if (stringState) {
-                        // Deleting string character at beginning of token index
-                        String string1 = new StringBuilder(string).deleteCharAt(0).toString();
-                        ArrayList<String> string_token = new ArrayList<>();
-                        string_token.add("String: " + string1);
-                        tokens.add(string_token);
-                        string = "";
-                        string_token = null;
-                        stringState = false;
-                    } else {
-                        stringState = true;
-                    } break;
+            if (chr == '"') {// If stringState is true...
+                if (stringState) {
+                    // Deleting string character at beginning of token index
+                    String string1 = new StringBuilder(string).deleteCharAt(0).toString();
+                    ArrayList<String> string_token = new ArrayList<>();
+                    string_token.add("String: " + string1);
+                    tokens.add(string_token);
+                    string = "";
+                    string_token = null;
+                    stringState = false;
+                } else {
+                    stringState = true;
+                }
             }
+
             // Build up our string
-            if (stringState) string += chr;
+            if (stringState) {
+                string += chr;
+            } else {
+                patternMatcher("Parenthesis(Open)", "(", chr); patternMatcher("Parenthesis(Close)", ")", chr);
+                patternMatcher("ScopeDef(Open)", "{", chr);    patternMatcher("ScopeDef(Close)", "}", chr);
+                patternMatcher("Operator", "+-/*^%", chr);     patternMatcher("Separator", ",", chr);
+                patternMatcher("Statement End", ";", chr);     patternMatcher("Separator", ",", chr);
+                patternMatcher("Comparison", "<>", chr);       patternMatcher("Boolean Logic", "|&!", chr);
+            }
+
+            // tokens.add(tempArrayList);
         }
 
         // To find keywords
 
         return tokens;
 
+    }
+
+    public static boolean patternMatcher(String valueName, String pattern, char item) {
+        ArrayList<String> tempArrayList = new ArrayList<>();
+        if (pattern.contains(Character.toString(item))) {
+            tempArrayList.add(valueName + ": " + Character.toString(item));
+            tokens.add(tempArrayList);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[]args) throws Exception{
